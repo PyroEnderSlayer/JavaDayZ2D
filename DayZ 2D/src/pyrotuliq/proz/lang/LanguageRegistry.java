e2a2d630e902604cebaa3554fa6135c6d3b94913
PyroEnderSlayer <pyroenderslayer@gmail.com>
2014-05-15 04:07:13 +0200

    Update language system (functional).
    Changed LanguageRegistry and Language, and made it functional

--- DayZ 2D/src/pyrotuliq/proz/lang/LanguageRegistry.java
+++ DayZ 2D/src/pyrotuliq/proz/lang/LanguageRegistry.java
@@ -2,15 +2,51 @@ package pyrotuliq.proz.lang;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import pyrotuliq.proz.ProZ;
 
 public class LanguageRegistry {
-	private static Map<String, Language> langs;
+	private static Map<String, Language> langs = new HashMap<String, Language>();
+	private static Language currentLanguage;
+	
+	private LanguageRegistry() {
+		// DO NOTHING
+	}
 	
 	public static void loadLanguage(String lang) throws IOException {
+		Language language = new Language();
 		BufferedInputStream input = new BufferedInputStream(ProZ.class.getClassLoader().getResourceAsStream("assets/lang/" + lang));
-		input.read();
+		long available = input.available();
+		
+		String currentKey = "";
+		String currentValue = null;
+		for (int iterator = 0; iterator < available; iterator++) {
+			int b = input.read();
+			String inputString = String.valueOf(Character.toChars(b));
+			
+			if (inputString.equals("=") && currentValue == null)
+				currentValue = "";
+			else if (inputString.equals("\n") && currentValue != null) {
+				language.setKey(currentKey, currentValue);
+				currentKey = "";
+				currentValue = null;
+			} else if (currentValue != null)
+				currentValue += inputString;
+			else
+				currentKey += inputString;
+		}
+		
+		input.close();
+		langs.put(lang, language);
+		
+		if (currentLanguage == null)
+			currentLanguage = language;
+	}
+	
+	public static void setLanguage(String lang) {
+		if (langs.containsKey(lang))
+			currentLanguage = langs.get(lang);
 	}
 }
