ded5f53a2f1c4a093843cfadf37ae92100a31a02
PyroEnderSlayer <pyroenderslayer@gmail.com>
2014-05-17 22:41:25 +0200

    Add serialVersionUID to IDConflictError.class

--- DayZ 2D/src/pyrotuliq/proz/lang/IDConflictError.java
+++ DayZ 2D/src/pyrotuliq/proz/lang/IDConflictError.java
@@ -1,6 +1,8 @@
 package pyrotuliq.proz.lang;
 
 public class IDConflictError extends Error {
+	private static final long serialVersionUID = -2095581618591019720L;
+
 	public IDConflictError() {
 		super();
 	}
